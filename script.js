8e2b5894a33779216b731027d8eabb54b983e42f
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2024-09-23 18:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 //your code here
 const resetBtn = document.getElementById('reset');
-const resetBtn = document.getElementById('verify');
+const verify = document.getElementById('verify');
+const h3 = document.getElementById('h');
+const para = document.getElementById('para');
 const div = document.querySelector("div")
-/// render sixth Image randomly
+/// render sixth Image randomly.
 const newImgElement = document.createElement("img");
 const imgClasses = ["img1","img2","img3","img4","img5"];
-let randomIndex = Math.floor(Math.random()*imageClasses.length);
+let randomIndex = Math.floor(Math.random()*imgClasses.length);
 const randomImgClass = imgClasses[randomIndex];
 newImgElement.className = randomImgClass;
 div.append(newImgElement)
+//sixth random image generated successfully.
+
 
 
 
