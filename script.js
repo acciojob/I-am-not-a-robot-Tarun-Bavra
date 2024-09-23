b0870a88cd536133f0ab28b8bca25400c5cb2f19
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2024-09-23 05:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const imgClasses = ["img1","img2","img3","img4","img5"];
+const images = document.querySelectorAll("img");
+let randomIndex = Math.floor(Math.random()*images.length);
+const resetBtn = document.getElementById('reset');
+const newImg = document.createElement("img");
+newImg.className = imgClasses[randomIndex];
+resetBtn.addEventListener("click",resetHandler);
+function resetHandler(){
+	
+}
